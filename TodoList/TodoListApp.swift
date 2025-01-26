e7eb41234bed291d99842be8157f344f2e4a30fe
sarmadnawaz1 <muhammadsarmadnawaz@gmail.com>
2025-01-26 19:02:36 +0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by sarmad on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
